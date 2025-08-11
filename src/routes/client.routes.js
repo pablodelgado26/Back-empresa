11ad24cf6d1cc586277f05e847675f7ddda8f583
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-08-11 17:43:50 -0300

    feat: melhorias de rotas

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -4,19 +4,19 @@ import clientController from "../controllers/clientController.js";
 const clientRouter = express.Router();
 
 // Rotas de Clientes
-// GET /clientes - Listar todos os Clientes
+// GET /clients - Listar todos os Clientes
 clientRouter.get("/", clientController.getAllClients);
 
-// GET /clientes/:id - Obter um Cliente pelo ID
+// GET /clients/:id - Obter um Cliente pelo ID
 clientRouter.get("/:id", clientController.getClientById);
 
-// POST /clientes - Criar um novo Cliente
+// POST /clients - Criar um novo Cliente
 clientRouter.post("/", clientController.createClient);
 
-// PUT /clientes/:id - Atualizar um Cliente existente
+// PUT /clients/:id - Atualizar um Cliente existente
 clientRouter.put("/:id", clientController.updateClient);
 
-// DELETE /clientes/:id - Remover um Cliente
+// DELETE /clients/:id - Remover um Cliente
 clientRouter.delete("/:id", clientController.deleteClient);
 
 export default clientRouter;
